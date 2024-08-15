e9fa6a2a071760b5e8a5ec5d137b641a09f59400
mario.juez <mario.juez@fsfb.org.co>
2024-08-15 18:47:52 -0500

    progress refactor MenuSticky

--- src/layout/MenuBar-2/scripts/MenuSticky.js
+++ src/layout/MenuBar-2/scripts/MenuSticky.js
@@ -24,31 +24,7 @@ const run = () => {
     multiListenWindowEvents(eventsListener, () => {
         //  responsive design mobile 
             if (window.innerWidth < 730) {
-                
-                const currentScrollY = document['documentElement' || 'body'].scrollTop;
-                const currentPositionScroll = currentScrollY - previousScrollY;
-
-                previousScrollY = currentScrollY;
-                offset += currentPositionScroll;
-
-                const navbarHeight = navbar.clientHeight;
-
-                // 1. the navbar is not visible
-                if (offset > navbarHeight) {
-                    offset = navbarHeight;
-                }
-
-                // 2. the navbar is visible and I exceed the height of the navbar when slide up scroll
-                if (offset < 0) {
-                    offset = 0;
-                }
-
-                // elements DOM to modify
-                navbar.style.top = `${-(offset)}px`;
-                buttonSticky.style.top = `${ (navbarHeight - offset) - 1 }px`;
-                menuList.style.top = `${ (navbarHeight + buttonSticky.clientHeight) - 1 }px`;
-
-
+               makeMenuSticky();
             } else {
                 // remove property top when is full screen
                 navbar.style.removeProperty('top');
@@ -64,8 +40,30 @@ const multiListenWindowEvents = (eventsListener, callback) => {
     });
 };
 
-const makeMenuSticky = (  elementSticky )  => {
+const makeMenuSticky = ()  => {
 
+    const currentScrollY = document['documentElement' || 'body'].scrollTop;
+    const currentPositionScroll = currentScrollY - previousScrollY;
+
+    previousScrollY = currentScrollY;
+    offset += currentPositionScroll;
+
+    const navbarHeight = navbar.clientHeight;
+
+    // 1. the navbar is not visible
+    if (offset > navbarHeight) {
+        offset = navbarHeight;
+    }
+
+    // 2. the navbar is visible and I exceed the height of the navbar when slide up scroll
+    if (offset < 0) {
+        offset = 0;
+    }
+
+    // elements DOM to modify
+    navbar.style.top = `${-(offset)}px`;
+    buttonSticky.style.top = `${ (navbarHeight - offset) - 1 }px`;
+    menuList.style.top = `${ (navbarHeight + buttonSticky.clientHeight) - 1 }px`;
 }
 
 export default {
