92388ff70f04fc20423958a7725b52464fb63600
mario.juez <mario.juez@fsfb.org.co>
2024-08-15 20:08:30 -0500

    fix rename vars button hamburger

--- src/layout/MenuBar-2/scripts/ButtonHamburger.js
+++ src/layout/MenuBar-2/scripts/ButtonHamburger.js
@@ -4,7 +4,7 @@ const run = () =>{
     const buttonHamburger = document.querySelector(".button-hamburger");
     const menuList = document.querySelector(".menu");
     
-    menuHamburger.addEventListener("click", () => {
+    buttonHamburger.addEventListener("click", () => {
         buttonHamburger.classList.toggle("open");
         menuList.classList.toggle("show-nav");
     });
